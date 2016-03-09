43741333ffd213b8ac8e7d8d2d6df6f043966eef
Voydwalker <dj_didko_di@abv.bg>
2016-03-09 16:54:36 +0100

    ajax call works.writing to file experiment

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
+
 app = Flask(__name__)
 
 
 
 @app.route('/')
-def message():
+def default():
     return app.send_static_file('index.html')
 
+
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    with open('data.txt', 'a') as file:
+        json.dump(request.json, file)
+    return "logged"
 
 @app.route('/history')
 def history():
     return ''
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8080)
