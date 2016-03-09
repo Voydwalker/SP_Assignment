91a8585e948cc97484f2fba91a66bbc20f83f6b4
Voydwalker <dj_didko_di@abv.bg>
2016-03-09 14:32:47 +0100

    trying to figure out AJAX

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def mesage():
+def message():
     return app.send_static_file('index.html')
 
 
 @app.route('/history')
 def history():
-    return 'his'
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
