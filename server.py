7633211356459d9ff6f2dfa384f2903a05dfc68a
Voydwalker <dj_didko_di@abv.bg>
2016-03-10 17:06:20 +0100

    functionality works.needs polishing

--- server.py
+++ server.py
@@ -1,32 +1,26 @@
 from flask import Flask, request
 import json
 
-
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def default():
     return app.send_static_file('index.html')
-
 
 @app.route('/message', methods=['GET', 'POST'])
 def message():
     with open('data.txt', 'a') as file:
         json.dump(request.json, file)
         file.write("\n")
-    file.close()
-    return "logged"
+    return app.send_static_file('index.html')
 
 @app.route('/history')
 def history():
-    out=''
+    out='['
     with open('data.txt', 'r') as file:
         for line in file:
-            out+=(line+'<br>')
-    file.close()
-    return out
+            out+=(line+',')
+    return out[:-1]+']'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
