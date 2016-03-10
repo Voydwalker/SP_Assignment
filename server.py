6d357e37676eac9b0676bf69bb1fb5e391429bce
Voydwalker <dj_didko_di@abv.bg>
2016-03-10 16:18:32 +0100

    history works.timer works

--- server.py
+++ server.py
@@ -16,12 +16,17 @@ def message():
     with open('data.txt', 'a') as file:
         json.dump(request.json, file)
         file.write("\n")
-        file.close()
+    file.close()
     return "logged"
 
 @app.route('/history')
 def history():
-    return ''
+    out=''
+    with open('data.txt', 'r') as file:
+        for line in file:
+            out+=(line+'<br>')
+    file.close()
+    return out
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
